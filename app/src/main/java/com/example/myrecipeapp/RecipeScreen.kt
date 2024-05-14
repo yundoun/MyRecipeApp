4f112ed35795fe50e4dff3c4254bbff05f95e98d
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2024-05-14 17:21:14 +0900

    Feat : Detail Logic 구현

--- app/src/main/java/com/example/myrecipeapp/RecipeScreen.kt
+++ app/src/main/java/com/example/myrecipeapp/RecipeScreen.kt
@@ -1,6 +1,7 @@
 package com.example.myrecipeapp
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
@@ -23,58 +24,77 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberAsyncImagePainter
 
 @Composable
-fun RecipeScreen(modifier : Modifier = Modifier ){
-    val recipeViewModel : MainViewModel= viewModel()
+fun RecipeScreen(
+    modifier: Modifier = Modifier,
+    navigateToDetail: (Category) -> Unit
+) {
+    val recipeViewModel: MainViewModel = viewModel()
     val viewstate by recipeViewModel.categoryState
-    Box(modifier = Modifier.fillMaxSize()){
-        when{
-            viewstate.loading ->{
+    Box(modifier = Modifier.fillMaxSize()) {
+        when {
+            viewstate.loading -> {
                 CircularProgressIndicator(modifier.align(Alignment.Center))
             }
 
             viewstate.error != null -> {
                 Text("Error ${viewstate.error}")
             }
-            else ->{
+
+            else -> {
                 //Display Categories
-                CategoryScreen(categories = viewstate.list)
+                CategoryScreen(
+                    categories = viewstate.list,
+                    navigateToDetail
+                )
             }
         }
     }
 }
 
 @Composable
-fun CategoryScreen(categories: List<Category>){
-    LazyVerticalGrid(GridCells.Fixed(2), modifier = Modifier.fillMaxSize()){
-        items(categories){
-            category ->
-            CategoryItem(category = category)
+fun CategoryScreen(
+    categories: List<Category>,
+    navigateToDetail: (Category) -> Unit
+) {
+    LazyVerticalGrid(GridCells.Fixed(2), modifier = Modifier.fillMaxSize()) {
+        items(categories) { category ->
+            CategoryItem(category = category, navigateToDetail)
         }
     }
 }
 
 //
 @Composable
-fun CategoryItem(category: Category){
-    Column(modifier = Modifier
-        .padding(8.dp)
-        .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally)
+fun CategoryItem(
+    category: Category,
+    navigateToDetail: (Category) -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxSize()
+            .clickable { navigateToDetail(category) },
+
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
     {
 
         Image(
             // 비동기적으로 가져옴
             painter = rememberAsyncImagePainter(category.strCategoryThumb),
             contentDescription = null,
-            modifier = Modifier.fillMaxSize().aspectRatio(1f)
+            modifier = Modifier
+                .fillMaxSize()
+                .aspectRatio(1f)
         )
 
 
 
-        Text(text = category.strCategory,
+        Text(
+            text = category.strCategory,
             color = Color.Black,
             style = TextStyle(fontWeight = FontWeight.Bold),
-            modifier = Modifier.padding(top=4.dp)
+            modifier = Modifier.padding(top = 4.dp)
         )
     }
 }
